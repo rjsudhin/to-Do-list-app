9262a4e7d04865cefdbcc12f58f9cbc996204348
rjsudhin <rjsudhin@users.noreplay.githut.com>
2025-05-19 12:14:44 +0530

    to-do list is now working fine

--- js/app.js
+++ js/app.js
@@ -5,26 +5,32 @@ const userInput = document.querySelector('#user-input')
 
 // adding new wish
 addingBtn.addEventListener('mousedown', () => {
-  if (toDo.childElementCount < 5) {
-    // creating a li element for contain
-    let wishContainer = document.createElement('li')
-    // creating a checkbox
-    let checkbox = document.createElement('input')
-    checkbox.setAttribute('type', 'checkbox')
-    // this is the wish user entere 
-    let labelText = document.createElement('label')
-    labelText.textContent = userInput.value
-    let deleteBtn = document.createElement('button')
-    deleteBtn.classList.add('btn-delete')
-    deleteBtn.textContent = 'delete'
-    deleteBtn.addEventListener('click', removeContainer) 
-
-    wishContainer.appendChild(checkbox)
-    wishContainer.appendChild(labelText)
-    wishContainer.appendChild(deleteBtn)
-    toDo.appendChild(wishContainer)
+  if (userInput.value.length <= 20 && userInput.value != '') {
+    if (toDo.childElementCount < 5) {
+      // creating a li element for contain
+      let wishContainer = document.createElement('li')
+      // creating a checkbox
+      let checkbox = document.createElement('input')
+      checkbox.setAttribute('type', 'checkbox')
+      checkbox.addEventListener('click', makeItDone)
+      // this is the wish user entere 
+      let labelText = document.createElement('label')
+      labelText.textContent = userInput.value
+      let deleteBtn = document.createElement('button')
+      deleteBtn.classList.add('btn-delete')
+      deleteBtn.textContent = 'delete'
+      deleteBtn.addEventListener('click', removeContainer) 
+  
+      wishContainer.appendChild(checkbox)
+      wishContainer.appendChild(labelText)
+      wishContainer.appendChild(deleteBtn)
+      toDo.appendChild(wishContainer)
+      userInput.value = ''
+      userInput.focus()
+    }
+  } else {
+    console.log('put some little wishes or adding something')
   }
-
 })
 
 
@@ -32,4 +38,10 @@ function removeContainer(e) {
   let container = e.target.parentElement
   toDo.removeChild(container)
   console.log(container)
+}
+
+
+function makeItDone(e) {
+  let targetElement = e.target
+  targetElement.nextElementSibling.classList.toggle('checked')
 }
